3716f70800a980caee529e1d0c9ed25b1db0904a
Fabrício Vianna <104432845+FabricioViannaSM@users.noreply.github.com>
2025-06-05 17:57:23 -0300

    commit 20

---
+++ Ex020/script.js
@@ -0,0 +1,10 @@
+function dirigir(idade){
+    if(idade>= 18){
+        console.log("Pode entrar na auto escola.")
+    }else{
+        console.log("Nã pode entrar na auto escola.")
+    }
+}
+
+dirigir(19)
+dirigir(17)
