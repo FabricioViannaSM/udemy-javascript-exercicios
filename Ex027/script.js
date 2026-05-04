3840a4c09fc99aa232241c2014716673fd964762
Fabrício Vianna <104432845+FabricioViannaSM@users.noreply.github.com>
2026-05-04 20:04:18 -0300

    commit 153

---
+++ Ex027/script.js
@@ -0,0 +1,6 @@
+let arr =[1, 2, 3, 4]
+let bool = [true, false]
+
+
+console.log(arr.length)
+console.log(bool.length)
---
+++ Ex030/script.js
@@ -0,0 +1,7 @@
+let nomes = ["Jardel", "Mauricio", "Fabrício", "Carlos", "Salomão"]
+
+if (nomes.includes("Fabrício")){
+    console.log("O nome Fabrício estána lista")
+}else{
+    console.log("O nome Fabrício não está na lista")
+}
